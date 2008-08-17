1970db6a30ced92c78ff10b3bd2ab21b41fba994
Andreas Brachold <vdr07@deltab.de>
2008-08-17 10:01:48 +0000

    * update to build with ffmpeg-svn (r14788)

--- contrib/vdr2jpeg/ffm.cpp
+++ contrib/vdr2jpeg/ffm.cpp
@@ -20,10 +20,10 @@
 #include "ffm.h"
 
 extern "C" {
-#include <avutil.h>
-#include <avcodec.h>
-#include <avformat.h>
-#include <swscale.h>
+#include <libavutil/avutil.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
 }
 
 static int frame_width = 0;
