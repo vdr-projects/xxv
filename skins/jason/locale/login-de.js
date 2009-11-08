2c7bdeb28a9f56ddc0c998eb6229dcf25d04ceee
Andreas Brachold <vdr07@deltab.de>
2009-11-08 16:29:23 +0000

    * AJAX: adjust message response parsing {data:"msg",success=true} * jason: add form based login

---
+++ skins/jason/locale/login-de.js
@@ -0,0 +1,19 @@
+/*
+ * jason - Javascript based skin for xxv
+ * Copyright(c) 2009, anbr
+ * 
+ * http://xxv.berlios.de/
+ *
+ * $Id$
+ *
+ * German translation
+ */
+
+/* login.js */
+Ext.LoginPanel.prototype.szTitle = 'Bitte anmelden';
+Ext.LoginPanel.prototype.szUser = 'Anwender';
+Ext.LoginPanel.prototype.szPassword = 'Passwort';
+Ext.LoginPanel.prototype.szLogin = 'Anmelden';
+Ext.LoginPanel.prototype.szFailed = 'Anmeldung fehlgeschlagen!';
+Ext.LoginPanel.prototype.szWarning = 'Warnung!';
+Ext.LoginPanel.prototype.szUnreachable = 'Server zur Authentifizierung ist nicht erreichtbar : ';
---
+++ skins/jason/locale/login-it.js
@@ -0,0 +1,19 @@
+/*
+ * jason - Javascript based skin for xxv
+ * Copyright(c) 2009, anbr
+ * 
+ * http://xxv.berlios.de/
+ *
+ * $Id$
+ *
+ * Template for translation
+ */
+
+/* login.js */
+Ext.LoginPanel.prototype.szTitle = 'Please login';
+Ext.LoginPanel.prototype.szUser = 'Username';
+Ext.LoginPanel.prototype.szPassword = 'Password';
+Ext.LoginPanel.prototype.szLogin = 'Login';
+Ext.LoginPanel.prototype.szFailed = 'Login failed!';
+Ext.LoginPanel.prototype.szWarning = 'Warning!';
+Ext.LoginPanel.prototype.szUnreachable = 'Authentication server is unreachable : ';
